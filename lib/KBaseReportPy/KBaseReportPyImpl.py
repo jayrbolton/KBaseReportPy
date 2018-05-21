dddfa168cedb9ae2a8dfb6f66c7d4d436acfcaaf
Jay R Bolton <jayrbolton@gmail.com>
2018-05-21 11:02:15 -0700

    Split the KIDL spec into an API spec for the catalog and a Workspace spec

--- lib/KBaseReportPy/KBaseReportPyImpl.py
+++ lib/KBaseReportPy/KBaseReportPyImpl.py
@@ -24,7 +24,7 @@ class KBaseReportPy:
     ######################################### noqa
     VERSION = "0.0.1"
     GIT_URL = "https://github.com/jayrbolton/KBaseReportPy"
-    GIT_COMMIT_HASH = "07a50ca5e635b0f794e9b8ad33c4228d5a11e537"
+    GIT_COMMIT_HASH = "b8ca60331fe769ae2cc3f0485e57bf8f7aea31fa"
 
     #BEGIN_CLASS_HEADER
     #END_CLASS_HEADER
@@ -45,55 +45,45 @@ class KBaseReportPy:
         Function signature for the create() method -- generate a report for an app run.
         create_extended() is the preferred method if you have html_links and
         file_links, but this is still provided for backwards compatibility.
+        @deprecated KBaseReportPy.create_extended_report
         :param params: instance of type "CreateParams" (* Parameters for the
            create() method * Pass in *either* workspace_name or workspace_id
-           -- only one is needed * Required arguments: *    int workspace_id
-           - needed if workspace_name is blank. Preferred as its immutable. *
-           string workspace_name - needed if workspace_id is blank. Note that
-           this may change. *    Report report) -> structure: parameter
-           "report" of type "Report" (* A simple Report of a method run in
-           KBase. * Provides a fixed-width, text-based summary message, a
-           list of warnings, * and a list of objects created. * Required
-           arguments: *     string text_message - Readable plain-text report
-           message * @optional warnings file_links html_links direct_html
-           direct_html_link_index * @metadata ws length(warnings) as Warnings
-           * @metadata ws length(text_message) as Message Length * @metadata
-           ws length(objects_created) as Objects Created) -> structure:
-           parameter "text_message" of String, parameter "warnings" of list
-           of String, parameter "objects_created" of list of type
-           "WorkspaceObject" (* Represents a Workspace object with some brief
-           description text * that can be associated with the object. *
-           Required arguments: *     ws_id ref - workspace ID * @optional
-           description) -> structure: parameter "ref" of type "ws_id" (*
-           Workspace ID reference - eg. 'ws/id/ver' * @id ws), parameter
-           "description" of String, parameter "file_links" of list of type
-           "LinkedFile" (* Represents a file or html archive that the report
-           links to * Used in Report and the create() function * Required
-           arguments: *     handle_ref handle - Handle ID *     string name -
-           Plain-text name of the file (shown to the user) *     string URL -
-           URL of shock node: <shock-url>/node/<shock-handle-id> * @optional
-           description label) -> structure: parameter "handle" of type
-           "handle_ref" (* Reference to a handle ID * @id handle), parameter
-           "description" of String, parameter "name" of String, parameter
-           "label" of String, parameter "URL" of String, parameter
-           "html_links" of list of type "LinkedFile" (* Represents a file or
-           html archive that the report links to * Used in Report and the
-           create() function * Required arguments: *     handle_ref handle -
-           Handle ID *     string name - Plain-text name of the file (shown
-           to the user) *     string URL - URL of shock node:
-           <shock-url>/node/<shock-handle-id> * @optional description label)
-           -> structure: parameter "handle" of type "handle_ref" (* Reference
-           to a handle ID * @id handle), parameter "description" of String,
-           parameter "name" of String, parameter "label" of String, parameter
-           "URL" of String, parameter "direct_html" of String, parameter
-           "direct_html_link_index" of Long, parameter "workspace_name" of
-           String, parameter "workspace_id" of Long
+           -- only one is needed * Required arguments: *     SimpleReport
+           report *     string workspace_name - needed if workspace_id is
+           blank. Note that this may change. *     int workspace_id - needed
+           if workspace_name is blank. Preferred as it is immutable.) ->
+           structure: parameter "report" of type "SimpleReport" (* A simple
+           report for use in create() * Required arguments: *     string
+           text_message - Readable plain-text report message * Optional
+           arguments: *     list<WorkspaceObject> objects_created - List of
+           result workspace objects that this app *         has created. They
+           will get linked in the report view. *     list<string> warnings -
+           A list of plain-text warning messages *     string direct_html -
+           simple html text that will be rendered within the report widget *
+           @optional text_message warnings objects_created * @metadata ws
+           length(warnings) as Warnings * @metadata ws length(text_message)
+           as Message Length * @metadata ws length(objects_created) as
+           Objects Created) -> structure: parameter "text_message" of String,
+           parameter "warnings" of list of String, parameter
+           "objects_created" of list of type "WorkspaceObject" (* Represents
+           a Workspace object with some brief description text * that can be
+           associated with the object. * Required arguments: *     ws_id ref
+           - workspace ID * Optional arguments: *     string description - A
+           plaintext, human-readable description of the *         object
+           created. * @optional description) -> structure: parameter "ref" of
+           type "ws_id" (* Workspace ID reference - eg. 'ws/id/ver' * @id
+           ws), parameter "description" of String, parameter "workspace_name"
+           of String, parameter "workspace_id" of Long
         :returns: instance of type "ReportInfo" (* The reference to the saved
            KBaseReport. This is the return object for * both create() and
-           create_extended() * Required arguments: *    ws_id ref *    string
-           name) -> structure: parameter "ref" of type "ws_id" (* Workspace
-           ID reference - eg. 'ws/id/ver' * @id ws), parameter "name" of
-           String
+           create_extended() * Returned data: *    ws_id ref - reference to a
+           workspace object in the form of *       
+           workspace_id/object_id/version. This is a reference to the saved
+           data *        from creating the report. *    string name -
+           Plaintext unique name for the report. In *        create_extended,
+           this can optionally be set in a parameter.) -> structure:
+           parameter "ref" of type "ws_id" (* Workspace ID reference - eg.
+           'ws/id/ver' * @id ws), parameter "name" of String
         """
         # ctx is the context object
         # return variables are: info
@@ -126,63 +116,70 @@ class KBaseReportPy:
            be zipped and uploaded * int direct_html_link_index - Index in
            html_links to set the direct/default view in the *     report
            (ignored if direct_html is present). * string direct_html - simple
-           html text that will be rendered within the report widget *     (do
-           not use both this and html_links -- use one or the other) *
-           list<File> file_links - a list of file paths or shock node IDs.
-           Allows the user to *     specify files that the report widget
-           should link for download. * string report_object_name - name to
-           use for the report object *     (will be auto-generated if
-           unspecified) * html_window_height - fixed height in pixels of the
-           html window for the report * summary_window_height - fixed height
-           in pixels of the summary window for the report * string
-           workspace_name - name of workspace where object should be saved *
-           int workspace_id - id of workspace where object should be saved *
-           * @metadata ws length(warnings) as Warnings * @metadata ws
-           length(text_message) as Message Length * @metadata ws
-           length(objects_created) as Objects Created * @metadata ws
-           length(html_links) as HTML Links * @metadata ws length(file_links)
-           as File Links * @optional message objects_created warnings
-           html_links direct_html direct_html_link_index file_links
-           report_object_name html_window_height summary_window_height) ->
-           structure: parameter "message" of String, parameter
-           "objects_created" of list of type "WorkspaceObject" (* Represents
-           a Workspace object with some brief description text * that can be
-           associated with the object. * Required arguments: *     ws_id ref
-           - workspace ID * @optional description) -> structure: parameter
-           "ref" of type "ws_id" (* Workspace ID reference - eg. 'ws/id/ver'
-           * @id ws), parameter "description" of String, parameter "warnings"
-           of list of String, parameter "html_links" of list of type "File"
-           (* A file to be linked or uploaded for the report. In
-           extended_report(), * this will get converted into a lower-level
-           LinkedFile before uploading * Pass in *either* a shock_id or a
-           path. If a path is given, then the file * will be uploaded. *
-           Required arguments: *     string path - only if shock_id is
-           absent. Can be a file or dir path. *     string shock_id - only if
-           path is absent. *     string name - plain-text file name -- shown
-           to the user. * @optional description) -> structure: parameter
-           "path" of String, parameter "shock_id" of String, parameter "name"
-           of String, parameter "description" of String, parameter
-           "direct_html" of String, parameter "direct_html_link_index" of
-           Long, parameter "file_links" of list of type "File" (* A file to
-           be linked or uploaded for the report. In extended_report(), * this
-           will get converted into a lower-level LinkedFile before uploading
-           * Pass in *either* a shock_id or a path. If a path is given, then
-           the file * will be uploaded. * Required arguments: *     string
-           path - only if shock_id is absent. Can be a file or dir path. *   
-           string shock_id - only if path is absent. *     string name -
-           plain-text file name -- shown to the user. * @optional
-           description) -> structure: parameter "path" of String, parameter
-           "shock_id" of String, parameter "name" of String, parameter
-           "description" of String, parameter "report_object_name" of String,
-           parameter "html_window_height" of Double, parameter
+           html text that will be rendered within the report widget *     If
+           you pass in both direct_html and html_links, then direct_html will
+           be ignored. * list<File> file_links - a list of file paths or
+           shock node IDs. Allows the user to *     specify files that the
+           report widget should link for download. * string
+           report_object_name - name to use for the report object *     (will
+           be auto-generated if unspecified) * html_window_height - fixed
+           height in pixels of the html window for the report *
+           summary_window_height - fixed height in pixels of the summary
+           window for the report * string workspace_name - name of workspace
+           where object should be saved. *     Required if workspace_id is
+           not present. * int workspace_id - id of workspace where object
+           should be saved. *     Required if workspace_name is not present.
+           * * @optional message objects_created warnings html_links
+           direct_html direct_html_link_index file_links report_object_name
+           html_window_height summary_window_height) -> structure: parameter
+           "message" of String, parameter "objects_created" of list of type
+           "WorkspaceObject" (* Represents a Workspace object with some brief
+           description text * that can be associated with the object. *
+           Required arguments: *     ws_id ref - workspace ID * Optional
+           arguments: *     string description - A plaintext, human-readable
+           description of the *         object created. * @optional
+           description) -> structure: parameter "ref" of type "ws_id" (*
+           Workspace ID reference - eg. 'ws/id/ver' * @id ws), parameter
+           "description" of String, parameter "warnings" of list of String,
+           parameter "html_links" of list of type "File" (* A file to be
+           linked in the report. Pass in *either* a shock_id or a * path. If
+           a path to a file is given, then the file will be uploaded. If a *
+           path to a directory is given, then it will be zipped and uploaded.
+           * Required arguments: *     string path - Can be a file or
+           directory path. Required if shock_id is absent. *     string
+           shock_id - Shock node ID. Required if path is absent. *     string
+           name - Plain-text file name -- shown to the user. * Optional
+           arguments: *     string description - A plaintext, human-readable
+           description of the file. * @optional description) -> structure:
+           parameter "path" of String, parameter "shock_id" of String,
+           parameter "name" of String, parameter "description" of String,
+           parameter "direct_html" of String, parameter
+           "direct_html_link_index" of Long, parameter "file_links" of list
+           of type "File" (* A file to be linked in the report. Pass in
+           *either* a shock_id or a * path. If a path to a file is given,
+           then the file will be uploaded. If a * path to a directory is
+           given, then it will be zipped and uploaded. * Required arguments:
+           *     string path - Can be a file or directory path. Required if
+           shock_id is absent. *     string shock_id - Shock node ID.
+           Required if path is absent. *     string name - Plain-text file
+           name -- shown to the user. * Optional arguments: *     string
+           description - A plaintext, human-readable description of the file.
+           * @optional description) -> structure: parameter "path" of String,
+           parameter "shock_id" of String, parameter "name" of String,
+           parameter "description" of String, parameter "report_object_name"
+           of String, parameter "html_window_height" of Double, parameter
            "summary_window_height" of Double, parameter "workspace_name" of
            String, parameter "workspace_id" of Long
         :returns: instance of type "ReportInfo" (* The reference to the saved
            KBaseReport. This is the return object for * both create() and
-           create_extended() * Required arguments: *    ws_id ref *    string
-           name) -> structure: parameter "ref" of type "ws_id" (* Workspace
-           ID reference - eg. 'ws/id/ver' * @id ws), parameter "name" of
-           String
+           create_extended() * Returned data: *    ws_id ref - reference to a
+           workspace object in the form of *       
+           workspace_id/object_id/version. This is a reference to the saved
+           data *        from creating the report. *    string name -
+           Plaintext unique name for the report. In *        create_extended,
+           this can optionally be set in a parameter.) -> structure:
+           parameter "ref" of type "ws_id" (* Workspace ID reference - eg.
+           'ws/id/ver' * @id ws), parameter "name" of String
         """
         # ctx is the context object
         # return variables are: info
